6c28a49d4fdb4ea8a8e3b1e32c6d82c690ffca98
saad710 <ifrat@encoderit.net>
2023-01-30 10:56:05 +0600

    app-created

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
+
+module.exports = {
+  i18n: {
+    locales: ['fr'],
+    defaultLocale: 'fr',
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
+// import "@fontsource/tajawal";
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
