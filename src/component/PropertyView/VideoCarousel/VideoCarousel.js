1817b0cc412c381390c29419a62906d4a06d315e
encoderit-ifrat <114899708+encoderit-ifrat@users.noreply.github.com>
2024-07-27 16:51:48 +0600

    translate-no-videos

--- src/component/PropertyView/VideoCarousel/VideoCarousel.js
+++ src/component/PropertyView/VideoCarousel/VideoCarousel.js
@@ -1,88 +1,88 @@
 // import useWindowDimensions from "@/hooks/useCurrentDisplaySize";
-import React, { useState } from "react";
-import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
-import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
-import { Box, Stack, Typography } from "@mui/material";
-import zIndex from "@mui/material/styles/zIndex";
+import React, { useState } from 'react'
+import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined'
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined'
+import { Box, Stack, Typography } from '@mui/material'
+import zIndex from '@mui/material/styles/zIndex'
 
-const VideoCarousel = ({ videoLinks, ratio = "1 / 1" }) => {
-  console.log("🟥 ~ VideoCarousel ~ VideoCarousel:", videoLinks);
+const VideoCarousel = ({ videoLinks, ratio = '1 / 1' }) => {
+  console.log('🟥 ~ VideoCarousel ~ VideoCarousel:', videoLinks)
 
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0)
 
   // const { width } = useWindowDimensions();
 
   const onVideoEnd = () => {
     // Go to the next video when the current one ends
-    setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videoLinks.length);
-  };
+    setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videoLinks.length)
+  }
 
   return (
     <div
       className="video-carousel-container"
       style={{
-        position: "relative",
+        position: 'relative',
       }}
     >
       {videoLinks?.length > 0 && (
         <Box
           // className="nav-icons"
           style={{
-            position: "absolute",
-            bottom: "70px",
-            right: "20px",
-            display: "flex",
+            position: 'absolute',
+            bottom: '70px',
+            right: '20px',
+            display: 'flex',
             zIndex: 10,
           }}
         >
           {/* Navigation buttons */}
           <button
             style={{
-              background: "#F2F5F6",
-              borderRadius: "1111px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              padding: "10px",
-              cursor: "pointer",
-              border: "none",
+              background: '#F2F5F6',
+              borderRadius: '1111px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: '10px',
+              cursor: 'pointer',
+              border: 'none',
             }}
             onClick={() =>
               setCurrentVideoIndex(
                 (prevIndex) =>
-                  (prevIndex - 1 + videoLinks.length) % videoLinks.length
+                  (prevIndex - 1 + videoLinks.length) % videoLinks.length,
               )
             }
           >
             <ArrowBackIosOutlinedIcon
               sx={{
-                color: "grey",
-                fontSize: "16px",
+                color: 'grey',
+                fontSize: '16px',
               }}
             />
           </button>
           <button
             style={{
-              background: "#0362F0",
-              borderRadius: "1111px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              padding: "10px",
-              cursor: "pointer",
-              border: "none",
-              marginLeft: "10px",
+              background: '#0362F0',
+              borderRadius: '1111px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: '10px',
+              cursor: 'pointer',
+              border: 'none',
+              marginLeft: '10px',
             }}
             onClick={() =>
               setCurrentVideoIndex(
-                (prevIndex) => (prevIndex + 1) % videoLinks.length
+                (prevIndex) => (prevIndex + 1) % videoLinks.length,
               )
             }
           >
             <ArrowForwardIosOutlinedIcon
               sx={{
-                color: "#ffffff",
-                fontSize: "16px",
+                color: '#ffffff',
+                fontSize: '16px',
               }}
             />
           </button>
@@ -96,7 +96,7 @@ const VideoCarousel = ({ videoLinks, ratio = "1 / 1" }) => {
           style={{
             aspectRatio: ratio,
 
-            width: "100%",
+            width: '100%',
             // height: height,
           }}
           src={`https://www.youtube.com/embed/${videoLinks[currentVideoIndex]}?autoplay=1`}
@@ -107,27 +107,27 @@ const VideoCarousel = ({ videoLinks, ratio = "1 / 1" }) => {
       ) : (
         <Box
           sx={{
-            background: "#f1f1f1",
+            background: '#f1f1f1',
             aspectRatio: ratio,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
           <Typography
             variant="p"
             sx={{
-              color: " #7450F0",
-              fontWeight: "600",
-              fontSize: "20px",
+              color: ' #7450F0',
+              fontWeight: '600',
+              fontSize: '20px',
             }}
           >
-            No videos Found
+            Nenhum vídeo encontrado
           </Typography>
         </Box>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default VideoCarousel;
+export default VideoCarousel
